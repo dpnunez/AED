10957547b58fceb09373a35b16decea3f01932f9
Daniel Nunez <daniel.nunez.pessoal@gmail.com>
2022-03-16 18:08:32 -0300

    feat(2.3): add menu function

--- list-2/exercicio-3/index.c
+++ list-2/exercicio-3/index.c
@@ -2,6 +2,31 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define ADD_VALUE 1
+#define REMOVE_VALUE 2
+#define FIND_VALUE 3
+#define LIST_VALUE 4
+#define EXIT_VALUE 5
+
+void getOption(int *option);
+
 int main() {
+	int option;
+
+	do {
+		getOption(&option);
+	} while(1);
+
 	return 0;
+}
+
+void getOption(int *option) {
+	printf("[%d] - ADICIONAR CONTATO\n", ADD_VALUE);
+	printf("[%d] - REMOVER CONTACT\n", REMOVE_VALUE);
+	printf("[%d] - BUSCAR POR NOME\n", FIND_VALUE);
+	printf("[%d] - LISTAR\n", LIST_VALUE);
+	printf("[%d] - SAIR\n\n", EXIT_VALUE);
+
+	printf("Inserir opçao:");
+	scanf("%d", option);
 }
