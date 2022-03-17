8b418efa12d7beb44e07153109e571965e258acd
Daniel Nunez <daniel.nunez.pessoal@gmail.com>
2022-03-17 20:39:43 -0300

    feat(2.3): WIP - createContact function

--- list-2/exercicio-3/index.c
+++ list-2/exercicio-3/index.c
@@ -8,13 +8,40 @@
 #define LIST_VALUE 4
 #define EXIT_VALUE 5
 
+#define CONTACT_SIZE sizeof(int) * 2 + sizeof(char) * 10
+
 void getOption(int *option);
+void createContact(void **contact_list);
 
 int main() {
 	int option;
+	void *pBuffer = NULL;
+
+	pBuffer = (void *)malloc(1 * sizeof(int));
+
+	if(pBuffer == NULL) {
+		printf("Error ao tentar alocar a memória");
+		exit(1);
+	}
+
+	*(int *)pBuffer = 0; // pBuffer[0] = 0?
 
 	do {
 		getOption(&option);
+
+		switch(option) {
+			case ADD_VALUE:
+				createContact(&pBuffer);
+				break;
+			case REMOVE_VALUE:
+				break;
+			case FIND_VALUE:
+				break;
+			case LIST_VALUE:
+				break;
+			case EXIT_VALUE:
+				break;
+		}
 	} while(1);
 
 	return 0;
@@ -29,4 +56,16 @@ void getOption(int *option) {
 
 	printf("Inserir opçao:");
 	scanf("%d", option);
+}
+
+void createContact(void **contact_list) {
+	int contact_list_length = *(int *)(*contact_list);
+	int contact_list_size = sizeof(int) + (contact_list_length * CONTACT_SIZE);
+	int next_contact_list_size = sizeof(int) + ((contact_list_length + 1) * CONTACT_SIZE);
+
+	*contact_list = realloc(*contact_list, next_contact_list_size);
+
+	printf("\nNome:");
+	scanf("%s", (char *)(contact_list + contact_list_size));
+	printf("\n%s\n", (char *)(contact_list + contact_list_size));
 }
