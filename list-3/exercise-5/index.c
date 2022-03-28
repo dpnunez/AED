19f0b93f0d17ad23942b36c5862be658e6d1c87a
Daniel Nunez <daniel.nunez.pessoal@gmail.com>
2022-03-28 20:31:54 -0300

    feat(3.5): finish exercise 5

--- list-3/exercise-5/index.c
+++ list-3/exercise-5/index.c
@@ -14,7 +14,7 @@ typedef struct {
 
 
 void showMenu(int *pOption);
-void createPessoa(Pessoa *pBuffer, int *pCounter);
+void createPessoa(Pessoa **pBuffer, int *pCounter);
 void listPessoas(Pessoa *pBuffer, int listLength);
 
 int main() {
@@ -34,8 +34,8 @@ int main() {
 	do {
 		showMenu(&option);
 		if(option == EXIT_VALUE) break;
-		createPessoa(pBuffer, counter);
-		// listPessoas(pBuffer, *counter);
+		createPessoa(&pBuffer, counter);
+		// listPessoas(pBuffer, *counter); // To print list
 	} while(1);
 
 	free(counter);
@@ -48,24 +48,26 @@ void showMenu(int *pOption) {
 	scanf("%d", pOption);
 }
 
-void createPessoa(Pessoa *pBuffer, int *pCounter) {
+void createPessoa(Pessoa **pBuffer, int *pCounter) {
 	if(*pCounter == 0) {
-		pBuffer = malloc(sizeof(Pessoa));
+		*pBuffer = malloc(sizeof(Pessoa));
 	} else {
-		pBuffer = realloc(pBuffer, sizeof(Pessoa) * (*pCounter + 1));
+		*pBuffer = realloc(*pBuffer, sizeof(Pessoa) * (*pCounter + 1));
 	}
 
-	if(pBuffer == NULL) {
+	if(*pBuffer == NULL) {
 		printf("Erro ao tentar alocar memória");
 		exit(1);
 	}
 
+
+	// Sintaxe mais simples para acessar valores e endereços de ponteiros para ponteiros
 	printf("Nome: ");
-	scanf("%s", pBuffer[*pCounter].nome);
+	scanf("%s", (*pBuffer)[*pCounter].nome);
 	printf("Idade: ");
-	scanf("%d", &pBuffer[*pCounter].idade);
+	scanf("%d", &(*pBuffer)[*pCounter].idade);
 	printf("Altura: ");
-	scanf("%d", &pBuffer[*pCounter].altura);
+	scanf("%d", &(*pBuffer)[*pCounter].altura);
 
 	*pCounter += 1;
 }
