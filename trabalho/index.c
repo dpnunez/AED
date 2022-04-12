9572b6435cc84c9157ad1372a05acb560e631c27
Daniel Nunez <daniel.nunez.pessoal@gmail.com>
2022-04-11 21:01:08 -0300

    feat(trabalho): RESET function

--- trabalho/index.c
+++ trabalho/index.c
@@ -8,23 +8,44 @@
 #define LIST_VALUE 4
 #define EXIT_VALUE 5
 
+#define name_length 10
 
+#define option_size sizeof(int)
+#define counter_size sizeof(int)
+#define loop_counter_size sizeof(int)
+#define start_size sizeof(void *)
+#define end_size sizeof(void *)
+
+#define option_address 0
+#define counter_address (option_size)
+#define loop_counter_address (option_size + counter_size)
+#define start_address (option_size + counter_size + loop_counter_size)
+#define end_address (option_size + counter_size + loop_counter_size + end_size)
+
+
+void printPBuffer(void *pBuffer);
 void menu(int *pOption);
+void *getBufferRef(void *pBuffer, size_t data_address);
+void RESET(void *pBuffer);
+void PUSH(void *pBuffer);
+
 
 int main() {
-	int *option = NULL;
-	option = malloc(sizeof(int));
-	
-	if(!option) { 
-		printf("Erro ao alocar os dados iniciais");
-		return 1;
+	void *pBuffer = malloc(option_size + counter_size + loop_counter_size + start_size + end_size);
+
+	if(pBuffer == NULL) {
+		printf("Erro ao alocar pBuffer");
+		exit(1);
 	}
+	RESET(pBuffer);
+
+
+	// printPBuffer(pBuffer);
 
 
 	while(1) {
-		menu(option);
-
-		switch(*option) {
+		menu(pBuffer);
+		switch(*(int *)getBufferRef(pBuffer, option_address)) {
 			case ADD_VALUE:
 				// createContact(&pBuffer);
 				break;
@@ -42,11 +63,13 @@ int main() {
 				// return 0;
 				break;
 		}
-
-		printf("option é: %d", *option);
 	}
 }
 
+
+void *getBufferRef(void *pBuffer, size_t data_address) {
+	return pBuffer + data_address;
+}
 
 void menu(int *pOption) {
 	printf("[%d] - ADICIONAR CONTATO\n", ADD_VALUE);
@@ -56,4 +79,27 @@ void menu(int *pOption) {
 	printf("[%d] - SAIR\n\n", EXIT_VALUE);
 
 	scanf("%d", pOption);
+}
+
+
+void RESET(void *pBuffer) {
+	*(int *)getBufferRef(pBuffer, option_address) = 0;
+	*(int *)getBufferRef(pBuffer, counter_address) = 0;
+	*(int *)getBufferRef(pBuffer, loop_counter_address) = 0;
+	*(void **)getBufferRef(pBuffer, start_address) = NULL;
+	*(int **)getBufferRef(pBuffer, end_address) = NULL;
+}
+
+void PUSH(void *pBuffer) {	
+}
+
+
+void printPBuffer(void *pBuffer) {
+	printf("\n");
+	printf("option: %d\n", *(int *)getBufferRef(pBuffer, option_address));
+	printf("counter: %d\n", *(int *)getBufferRef(pBuffer, counter_address));
+	printf("loop: %d", *(int *)getBufferRef(pBuffer, loop_counter_address));
+	// printf("start: ", *(int *)getBufferRef(pBuffer, start_address));
+	// printf("end: ", *(int *)getBufferRef(pBuffer, end_address));
+	printf("\n\n");
 }
