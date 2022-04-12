2c9561b0302a67b5b419b2d4683ff1f20387faea
Daniel Nunez <daniel.nunez.pessoal@gmail.com>
2022-04-12 19:14:32 -0300

    feat(trabalho): add basic push structure

--- trabalho/index.c
+++ trabalho/index.c
@@ -10,17 +10,32 @@
 
 #define name_length 10
 
+
+// vars sizes
 #define option_size sizeof(int)
 #define counter_size sizeof(int)
 #define loop_counter_size sizeof(int)
 #define start_size sizeof(void *)
 #define end_size sizeof(void *)
+#define name_size ((name_length + 1) * sizeof(char))
+#define phone_size (sizeof(int))
+#define age_size (sizeof(int))
+#define next_size (sizeof(void *))
+#define previous_size (sizeof(void *))
 
+// pBuffer structure
 #define option_address 0
 #define counter_address (option_size)
 #define loop_counter_address (option_size + counter_size)
 #define start_address (option_size + counter_size + loop_counter_size)
 #define end_address (option_size + counter_size + loop_counter_size + end_size)
+
+// person structure
+#define name_address 0
+#define phone_address (name_size)
+#define age_address (name_size + phone_size)
+#define next_address (name_size + phone_size + age_size)
+#define previous_address (name_size + phone_size + age_size + next_address)
 
 
 void printPBuffer(void *pBuffer);
@@ -47,6 +62,7 @@ int main() {
 		menu(pBuffer);
 		switch(*(int *)getBufferRef(pBuffer, option_address)) {
 			case ADD_VALUE:
+				PUSH(pBuffer);
 				// createContact(&pBuffer);
 				break;
 			case REMOVE_VALUE:
@@ -90,7 +106,35 @@ void RESET(void *pBuffer) {
 	*(int **)getBufferRef(pBuffer, end_address) = NULL;
 }
 
-void PUSH(void *pBuffer) {	
+void PUSH(void *pBuffer) {
+	void *person = (void *)(malloc(name_size + phone_size + age_size + previous_size + next_size));
+
+	printf("\nInserir o nome: ");
+	scanf("%s", (char *)getBufferRef(person, name_address));
+
+	// Prever quando o nome estoura o person todo
+	while (strlen((char *)getBufferRef(person, name_address)) > 10) {
+    printf("*********************************************** \n");
+    printf("* Insira um nome com no máximo 10 caracteres! *\n");
+    printf("*********************************************** \n");
+    printf("Inserir o nome: ");
+    scanf("%s", (char *)getBufferRef(person, name_address));
+  }
+
+	printf("\nInserir a idade: ");
+	scanf("%s", (char *)getBufferRef(person, age_address));
+
+	printf("\nInserir a telefone: ");
+	scanf("%s", (char *)getBufferRef(person, phone_address));
+
+	if(*(int *)getBufferRef(pBuffer, start_address) == NULL) {
+		// Adicionando primeira pessoa;
+			*(void **)getBufferRef(person, next_address) = NULL;
+			*(void **)getBufferRef(person, next_address) = NULL;
+
+			*(void **)getBufferRef(pBuffer, start_address) = person;
+			*(void **)getBufferRef(pBuffer, end_address) = person;
+	}
 }
 
 
